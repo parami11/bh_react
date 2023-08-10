5bd2bc84810f8adad575a9933f6edec3567cbf95
parami <paramimodarage@gmail.com>
2023-08-10 21:34:48 +0200

    mock GET requests

--- bh_react/src/App.js
+++ bh_react/src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import { NewEmployee } from "./components/NewEmployee";
 import { EmployeeList } from "./components/EmployeeList";
 
-const initialArr = [{id:'1', firstName:'parami', lastName:'modarage', description:'test1'}, {id:'2', firstName:'malin', lastName:'desilva', description:'test2'}];
-
 function App() {
   return (
     <div className="App">
           <NewEmployee />
-    <EmployeeList employeeList={initialArr} />
+    <EmployeeList/>
     </div>
   );
 }
--- bh_react/src/components/EmployeeList.jsx
+++ bh_react/src/components/EmployeeList.jsx
@@ -1,12 +1,26 @@
 import { EmployeeItem } from "./EmployeeItem";
+import React, { useState, useEffect } from 'react';
 
-export function EmployeeList({employeeList}){
+export function EmployeeList(){
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+           .then((response) => response.json())
+           .then((data) => {
+              setEmployees(data);
+           })
+           .catch((err) => {
+              console.log(err.message);
+           });
+     }, []);
+     
     return(
         <div>
             <h4>Employee List</h4>
         <ul className="list">
             {
-                employeeList.map(employee => {
+                employees.map(employee => {
                     return (
                         <EmployeeItem key={employee.id} {...employee} />
                     )
